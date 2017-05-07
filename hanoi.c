ff19d741780a92e0f1e05161051218cafe5d66dc
Roy Wei <roywjy@gmail.com>
2017-05-07 18:28:58 +0800

    Fix styling

--- hanoi.c
+++ hanoi.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-void hanoi(int n, char A, char B, char C) {
-    if(n == 1) {
+void hanoi(int n, char A, char B, char C)
+{
+    if (n == 1)
+    {
         printf("Move sheet from %c to %c\n", A, C);
     }
-    else {
-        hanoi(n-1, A, C, B);
+    else
+    {
+        hanoi(n - 1, A, C, B);
         hanoi(1, A, B, C);
-        hanoi(n-1, B, A, C);
+        hanoi(n - 1, B, A, C);
     }
 }
 
-int main() {
+int main()
+{
     int n;
     printf("Enter the number of the plates: ");
     scanf("%d", &n);
